66240b81f673fa2b96dc404c30ea38c13abfa048
Miraculix75 <88479889+Miraculix75@users.noreply.github.com>
2024-06-05 17:40:35 +0200

    creat questions - JSON

---
+++ script.js
@@ -0,0 +1,64 @@
+let questions = [
+  {
+    question: "Wer hat HTML erfunden?",
+    "answer-1": "Robbie Williams",
+    "answer-2": "Lady Gaga",
+    "answer-3": "Tim Berners-Lee",
+    "answer-4": "Justin Bieber",
+    "correct-answer": 3,
+  },
+
+  {
+    question: "Wer hat JavaScript erfunden?",
+    "answer-1": "Steve Jobs",
+    "answer-2": "Robbie Williams",
+    "answer-3": "Lady Gaga",
+    "answer-4": "Justin Bieber",
+    "correct-answer": 1,
+  },
+
+  {
+    question: "Wer hat CSS erfunden?",
+    "answer-1": "Steve Jobs",
+    "answer-2": "Robbie Williams",
+    "answer-3": "Håkon Wium Lie",
+    "answer-4": "Justin Bieber",
+    "correct-answer": 3,
+  },
+
+  {
+    question: "Wer hat Bootstrap erfunden?",
+    "answer-1": "Steve Jobs",
+    "answer-2": "Robbie Williams",
+    "answer-3": "Lady Gaga",
+    "answer-4": "Mark Otto",
+    "correct-answer": 4,
+  },
+
+  {
+    question: "Wer hat PHP erfunden?",
+    "answer-1": "Steve Jobs",
+    "answer-2": "Rasmus Lerdorf",
+    "answer-3": "Lady Gaga",
+    "answer-4": "Justin Bieber",
+    "correct-answer": 2,
+  },
+
+  {
+    question: "Wer hat SQL erfunden?",
+    "answer-1": "Steve Jobs",
+    "answer-2": "Robbie Williams",
+    "answer-3": "Edgar F. Codd ",
+    "answer-4": "Justin Bieber",
+    "correct-answer": 3,
+  },
+
+  {
+    question: "Wer hat XML erfunden?",
+    "answer-1": "World Wide Web Consortium",
+    "answer-2": "Robbie Williams",
+    "answer-3": "Lady Gaga",
+    "answer-4": "Justin Bieber",
+    "correct-answer": 1,
+  }
+];
