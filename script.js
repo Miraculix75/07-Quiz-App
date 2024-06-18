733cb8b54ae911af072683c01143a972e8d04a2b
Miraculix75 <88479889+Miraculix75@users.noreply.github.com>
2024-06-18 18:54:30 +0200

    add sounds

--- script.js
+++ script.js
@@ -65,8 +65,11 @@ let questions = [
 
 
 let rightQuestions = 0;
-
 let currentQuestion = 0;
+
+let audio_success = new Audio('/audio/success.wav');
+let audio_wrong = new Audio('/audio/wrong.wav');
+
 
 function init() {
   document.getElementById("total-questions").innerHTML = questions.length;
@@ -91,13 +94,14 @@ function showQuestions() {
 
   } else {    // Show question
 
-    let percent = currentQuestion / question.length; 
+    let percent = (currentQuestion + 1) / questions.length; 
     percent = Math.round(percent * 100); 
+
+
     document.getElementById("progressBar").innerHTML = `${percent} %`;
-    // aktuelle Fragennummer / Gesamtfragenanzahl = Prozentwert für die Progressbar und dann * 100
+    document.getElementById("progressBar").style = `width: ${percent}%`;
+
     
-
-
     let question = questions[currentQuestion];
 
     document.getElementById("question-Number").innerHTML = currentQuestion + 1;
@@ -117,13 +121,15 @@ function answer(answer) {
   let idOfRightAnswer = `answer-${question['correct-answer']}`;
                         // + question[`correct-answer']`; anstelle vom ${} kann man auch das + verwenden.
 
-  if(selectedQuestionNumber == question['correct-answer']) {
+  if(selectedQuestionNumber == question['correct-answer']) { // Richtige Frage beantwortet
     
       document.getElementById('answer-' + selectedQuestionNumber).parentNode.classList.add('bg-success');
+      audio_success.play();
       rightQuestions++;
-  } else {
+  } else { // Falsche Antwort
       document.getElementById('answer-' + selectedQuestionNumber).parentNode.classList.add('bg-danger');
       document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success");
+      audio_wrong.play();
   }
 
   document.getElementById('next-Question').disabled = false;
@@ -154,3 +160,14 @@ function resetAnswerButtons() {
   document.getElementById("answer-4").parentNode.classList.remove("bg-success");
 }
 
+
+function restartGame() {
+  document.getElementById("headerImage").src = "img/pencil.jpg";
+  document.getElementById("questionBody").style = ""; // questionBody wieder anzeigen
+  document.getElementById("endScreen").style = "display: none"; // endScreen ausblenden
+
+  rightQuestions = 0;
+  currentQuestion = 0;
+
+  init();
+}
